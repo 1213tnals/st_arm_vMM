a08f8f1d4be1119b53d6cd8df7543e994fe50c18
palwanishan <palwanishan@gmail.com>
2023-01-12 19:31:47 +0900

    first commit

---
+++ st_arm_control/include/motor_controller.h
@@ -0,0 +1,50 @@
+#ifndef MOTOR_CONTROLLER_H
+#define MOTOR_CONTROLLER_H
+
+#include "shared_memory.h"
+#include "dynamics.h"
+#include "dynamixel.h"
+
+#define   tic2radL   2607.435432674516
+#define   tic2radX   10430.21970545193
+#define   window_size 20
+#define   num_of_dynamixels 7
+
+using Eigen::MatrixXd;
+
+
+class Motor_Controller{
+
+public:   
+  int count;
+  bool first_loop = true;
+
+  VectorXd th_joint = VectorXd::Zero(num_of_dynamixels);
+  VectorXd th_dot = VectorXd::Zero(num_of_dynamixels);
+  VectorXd th_dot_sma_filtered = VectorXd::Zero(num_of_dynamixels);
+  MatrixXd sma = MatrixXd::Zero(window_size, num_of_dynamixels);
+  VectorXd torque_wrist = VectorXd::Zero(num_of_dynamixels); 
+
+
+
+  Motor_Controller();
+  //~Motor_Controller();
+
+  VectorXd GetThetaX();
+  VectorXd GetThetaL();
+  VectorXd GetTheta();
+  VectorXd GetJointTheta();
+  VectorXd GetThetaDot();
+  VectorXd GetThetaDotEst();
+  VectorXd GetThetaDotSMAF();
+  VectorXd GetTorque();
+  void ReadTheta();    
+  void SetTorque(VectorXd tau);  
+  void SetPosition(VectorXd theta);  
+  void EnableMotor();
+  void EnableFilter();
+  void ReadCurrentLoopPI();
+};
+
+
+#endif // MOTOR_CONTROLLER_H
